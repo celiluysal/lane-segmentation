c76e996a7481469393f02a0cb318178e1e6ecd36
celiluysal <celiluysl15@gmail.com>
2020-09-16 23:14:50 +0300

    one hot encode and tensorize mask

--- src/preprocess.py
+++ src/preprocess.py
@@ -21,10 +21,9 @@ def tensorize_image(image_path, output_shape):
         batch_images.append(img)
 
     batch_images = np.array(batch_images)
-    # torch_image = torch.from_numpy(batch_images)
-    # image_tensor = torch_image.cuda()
-    # return image_tensor 
-    return batch_images 
+    torch_image = torch.from_numpy(batch_images)
+    image_tensor = torch_image.cuda()
+    return image_tensor 
     #[4765, 20, 20, 3] [B,W,H,C]
 
 def tensorize_mask(mask_path, output_shape ,n_class):
@@ -33,74 +32,60 @@ def tensorize_mask(mask_path, output_shape ,n_class):
         mask = cv2.imread(file_name, cv2.IMREAD_COLOR)
         mask = cv2.resize(mask, output_shape)
         mask = mask / 255
-        # print("mask", mask)
         mask = one_hot_encode(mask, n_class)
-        # print("mask enc", mask)
         batch_masks.append(mask)
   
     batch_masks = np.array(batch_masks)
-    # print("mask enc batch", batch_masks)
     torch_mask = torch.from_numpy(batch_masks)
-    # print("mask torch", torch_mask)
     mask_tensor = torch_mask.cuda()
-    # print("mask tens", mask_tensor)
-    # print("mask tens len", len(mask_tensor))
-    #[4765, 20, 20, 2] [B,W,H,C] 
+    #[4765, 20, 20, 3] [B,W,H,C] 
     return mask_tensor
 
 def one_hot_encode(data, n_class):
-    encoded_data = np.zeros((*data.shape, n_class), dtype=np.int) # (width, height, number_of_class)'lık bir array tanımlıyorum.    print(data)
-    print("1",encoded_data.shape)
+    encoded_data = np.zeros((data.shape[0], data.shape[1], n_class), dtype=np.int)
+    encoded_labels = []
     for lbl in range(n_class):
-        encoded_labels = [0] * n_class # buraya da [0, 0, .., 0] (n tane 0) bir encoded_labels, bizim durumumuz için [0, 0, 0] oluyor bu (n=3)
-        encoded_labels[lbl] = 1 # lbl = 0 için (arkaplan)    [1, 0, 0] labelini oluşturuyorum, 
-                                # lbl = 1 için (solid line)  [0, 1, 0] labelini oluşturuyorum.
-                                # lbl = 2 için (dashed line) [0, 0, 1] labelini oluşturuyorum.
-        print()
-        print(encoded_labels)
-
-        numerical_class_inds = np.empty(data.shape, dtype=bool)
-        print(type(numerical_class_inds))
-        print("nm", numerical_class_inds.shape)
-
-        for i in range(data.shape[0]):
-            for j in range(data.shape[1]):
-                for k in range(data.shape[2]):
-                    if (lbl == 0):
-                        numerical_class_inds[i][j][k] = data[i][j][k] == 0
-                    elif (lbl == 1):
-                        numerical_class_inds[i][j][k] = data[i][j][k] == 1 and k == 1
-                    elif (lbl == 2):
-                        numerical_class_inds[i][j][k] = data[i][j][k] == 1 and k == 2
-
-        # print(numerical_class_inds)
-        print("4",encoded_data.shape)
-        encoded_data[numerical_class_inds] = encoded_labels # lbl = 0 için tüm F'in sahip olduğu tüm w,h ikililerini [1, 0]'a eşitliyorum.
-                                                            # lbl = 1 için tüm O'un sahip olduğu tüm w,h ikililerini [0, 1]'e eşitliyorum.
-        print("5",encoded_data.shape)
-    # print(encoded_data)
+        encoded_label = [0] * n_class 
+        encoded_label[lbl] = 1
+        encoded_labels.append(encoded_label)
+    
+    black = np.array([0,0,0]) #black = background label=[1,0,0]
+    green = np.array([0,1,0]) #green = dashed line label=[0,1,0]
+    red = np.array([0,0,1]) #red = solid line label=[0,0,1]
+    
+    for i in range(data.shape[0]):
+        for j in range(data.shape[1]):
+            if ((data[i][j] == black).all()):
+                encoded_data[i, j] = encoded_labels[0]
+            elif ((data[i][j] == green).all()):
+                encoded_data[i, j] = encoded_labels[1]
+            elif ((data[i][j] == red).all()):
+                encoded_data[i, j] = encoded_labels[2]
     return encoded_data
 
 if __name__ == '__main__':
-    
-    # image_file_names = glob.glob(IMG_DIR + "/*")
-    # image_file_names.sort()
-    # batch_image_list = image_file_names[:5] #first n
-    # batch_image_tensor = tensorize_image(batch_image_list, (20,20))
-    
-    # print(batch_image_list)
-    # print(batch_image_tensor.dtype)
-    # print(type(batch_image_tensor))
-    # print(batch_image_tensor.shape)
+    start = time.time()
+    image_file_names = glob.glob(IMG_DIR + "/*")
+    image_file_names.sort()
+    batch_image_list = image_file_names[:50] #first n
+    batch_image_tensor = tensorize_image(batch_image_list, (220,220))
 
-    # print("------------")    
+    end = time.time()
+    print("image duration: ", (end - start) / 60)
+    print(batch_image_tensor.dtype)
+    print(type(batch_image_tensor))
+    print(batch_image_tensor.shape)
+
+    print("-----------------------")    
     
+    start = time.time()
     mask_file_names = glob.glob(MASK_DIR + "/*")
     mask_file_names.sort()
-    batch_mask_list = mask_file_names[:1] #first n
-    batch_mask_tensor = tensorize_mask(batch_mask_list, (5,5), 3)
-    
-    print(batch_mask_list)
+    batch_mask_list = mask_file_names[:50] #first n
+    batch_mask_tensor = tensorize_mask(batch_mask_list, (220,220), 3)
+
+    end = time.time()
+    print("mask duration: ", ((end - start)/60))
     print(batch_mask_tensor.dtype)
     print(type(batch_mask_tensor))
     print(batch_mask_tensor.shape)  
